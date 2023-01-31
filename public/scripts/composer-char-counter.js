c72d55ce7dab385e8282af3244e2913ac8d4a492
Connor <cgbroadfoot@gmail.com>
2023-01-31 18:23:55 +0000

    Updated character count to change the counter value displayed on the page. As the person types, the count goes down. If the count is negative, the counter goes red.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,8 +3,17 @@ $(document).ready(function() {
   let tweetText = document.getElementById('tweet-text')
 
   tweetText.addEventListener('input', function() {
+
+    const counter = document.getElementsByClassName('counter')
     let count = 140 - $(this).val().length
-    console.log(count)
+    $(counter).html(count)
+
+    if(count < 0) {
+      $(counter).css('color', 'red')
+    } 
+    if(count >= 0) {
+      $(counter).css('color', '')
+    }
   })
 
 });
