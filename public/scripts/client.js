0b2dd1cb6370438097ca1f097ee59f7a0d73e3e9
Connor <cgbroadfoot@gmail.com>
2023-02-01 18:14:08 +0000

    Updated the feed to dynamically display tweets based on sample data. The function that supports this can now take in an array of objects, rendering each as its own tweet.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,43 +4,50 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// 
+// Fake data taken from initial-tweets.json
+$(document).ready(function () {
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
-// Takes in a json tweet object
+function createTweetElement(obj) {
 
-function createTweetElement (obj) {
+  let newElement =
+    `<article class="tweet"><div class ="tweet-card"><header> <div>
+    <span class="avatar"><img src="${obj.user.avatars}"></span><span class="tweetBy">${obj.user.name}</span></div><div class="tweetByhandle">${obj.user.handle}</div></header><div class="tweet-content"><h4>${obj.content.text}</h4></div><hr><footer><div class="date">${obj.created_at}</div><div class="tweet-card-icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div></footer></div></article>`;
+  return newElement;
+};
 
-  $(document).ready(function () {
-    
-    let newElement = 
-    `<article class="tweet"><div class ="tweet-card"><header><div class="tweetBy">${obj.user.name}</div><div class="tweetByhandle">${obj.user.handle}</div></header><div class="tweet-content"><h4>${obj.content.text}</h4></div><hr><footer><div class="date">${obj.created_at}</div><div class="tweet-card-icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div></footer></div></article>`
-    return $('.feed').append(newElement); 
-  })
+function renderTweets(arr) {
+
+  arr.forEach((curr) => {
+    console.log(curr);
+    $('.feed').append(createTweetElement(curr));
+  });
 }
 
-// Returns a tweeter article element containing the entire html structure
-
-
-
-
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "JOJON",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-    },
-  "content": {
-    "text": "BABABOOYEE"
-    },
-  "created_at": 1461116232227
-}
-
-const $tweet = createTweetElement(tweetData)
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('.feed').append($tweet); 
+renderTweets(data);
+});
