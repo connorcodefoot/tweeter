f4453af2d36a976d9b10bdb4fa61a2523fc0aeb8
Connor <cgbroadfoot@gmail.com>
2023-02-03 05:05:37 +0000

    Added documentation

--- public/scripts/top-button.js
+++ public/scripts/top-button.js
@@ -1,22 +1,21 @@
 
+// Display a button in the rightside of the window for scrolling back to top
+
 $(document).ready(function () {
 
-  $(window).scroll(function() {
-    if($(window).scrollTop() > 600) {
-      $('#topBtn').css('visibility', 'visible')
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 600) {
+      $('#topBtn').css('visibility', 'visible');
     } else {
-      $('#topBtn').css('visibility', 'hidden')
+      $('#topBtn').css('visibility', 'hidden');
     }
+  });
+
+
+  // On click, scroll back to top
+  $('#topBtn').on('click', () => {
+    window.scrollTo(0, 0);
+    $('#topBtn').css('visibility', 'hidden');
+  });
+
 });
-
-
-  // On click, focus on text area new tweet
-$('#topBtn').on('click', () => {
-  window.scrollTo(0,0)
-  $('#topBtn').css('visibility', 'hidden')
-  $('#tweet-text').focus()
-  
-})
-
-
-})
