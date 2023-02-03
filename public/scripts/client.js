328c3a0b1a66c1a11baf9c651df467a55322c0c0
Connor <cgbroadfoot@gmail.com>
2023-02-03 05:05:08 +0000

    Added comments to describe functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,86 +1,89 @@
 
- // XSS prevention function
- 
- const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
+/************* New Tweet Form supporting functions  *************/
+// XSS prevention function. Receives a string from the tweet form. Returns a div wrapped string, evading xss.
+const escapeXss = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
   return div.innerHTML;
-  };
+};
 
-// Resets tweet form text and counter
+// Resets tweet form and character counter
 const formReset = () => {
-  $('#submit-tweet').trigger("reset");
+  $('#tweet-form').trigger("reset");
   $('.counter').html('140');
-}
+};
+
+// Validate form input. Checks for character count or empty field. Serves the user an error message if either errors occur and returns validation false, preventing the user submitting the tweet.
 
 const formValidation = () => {
-  if($('.counter').val() < 0) {
-    $('.error').html('Easy cowboy, that is too many characters').fadeOut(4000)
-    return false
+  if ($('.counter').val() < 0) {
+    $('.error').html('Easy cowboy, that is too many characters').fadeOut(4000);
+    return false;
   }
 
-  if($('.counter').val() === '140') {
-    $('.error').html('You tried to tweet nothing. Please add some content : )').fadeOut(4000)
-    return false
+  if ($('.counter').val() === '140') {
+    $('.error').html('You tried to tweet nothing. Please add some content : )').fadeOut(4000);
+    return false;
   }
 
-  return true
-}
+  return true;
+};
 
-
+/************* Core Functions  *************/
 $(document).ready(function () {
 
-
-  // Get all tweets stored in mem db and call render
+  // Get all tweets stored in mem db. Call renderTweets with return data. Print error to user if they fail to load. 
   function loadTweets() {
     $.get('/tweets')
-    .done((data) => {
-      renderTweets(data);
-    })
-    .fail(() => {
-      $('.feed').append('There was an error loading tweets')
-    })
+      .done((data) => {
+        renderTweets(data);
+      })
+      .fail(() => {
+        $('.feed').append('There was an error loading tweets');
+      });
 
   };
 
-  // Receives an object, outputs a block of html that uses data from the object.
+  // Takes in an array of data from loadTweets. Each element is created into a block of html through createTweetelement. The return data is then appended to the feed.
+  function renderTweets(data) {
+
+    const processedData = data.reverse();
+    processedData.forEach((curr) => {
+      $('.feed').append(createTweetElement(curr));
+    });
+  }
+
+  // Receives an object from renderTweets, outputs a block of html that uses data from the object.
   function createTweetElement(obj) {
 
     let newElement =
       `<article class="tweet">
-      <div class ="tweet-card">
-        <header>
+        <div class ="tweet-card">
+          <header>
             <span> <img src="${obj.user.avatars}"></span>
-          <div class="user-info">
-            <span>${obj.user.name}</span>
-            <span>${obj.user.handle}</span>
-          </div>
-        </header>
-        <div class="tweet-content">${escape(obj.content.text)}
+            <div class="user-info">
+              <span>${obj.user.name}</span>
+              <span>${obj.user.handle}</span>
+            </div>
+          </header>
+        <div class="tweet-content"><h4>${escapeXss(obj.content.text)}</h4>
         </div>
         <hr>
         <footer>
-        <div class="date">${timeago.format(obj.created_at)}</div>
-        <div class="tweet-card-icons">
-        <i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div></footer>
+          <div class="date">${timeago.format(obj.created_at)}</div>
+            <div class="tweet-card-icons">
+              <i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i>
+            </div>
+        </footer>
       </div>
     </article>`;
 
     return newElement;
   };
 
-  // Takes in an array of data. Each element is created into a block of html through createTweetelement. The return data is then appended to the feed.
-  function renderTweets(data) {
+  // Prevent default form submission for new tweets then submit serialized data (querystring) to server. Call form validation functions. Reset after submission. 
 
-    const processedData = data.reverse()
-    processedData.forEach((curr) => {
-      $('.feed').append(createTweetElement(curr));
-    });
-  }
-
-  // Prevent default form submission for new tweets then submit serialized data (querystring) to server. It also includes error messaging for empty tweets or a tweet that exceeds the # of characters available.
-  
-  $('#submit-tweet').submit(function (event) {
+  $('#tweet-form').submit(function (event) {
     if (event) {
       event.preventDefault();
     }
@@ -88,24 +91,16 @@ $(document).ready(function () {
     if (formValidation()) {
 
       $.post('/tweets', ($(this).serialize())).done(() => {
-       
-        $.get('/tweets').done((data) => {
-          $('.feed').prepend(createTweetElement((data[data.length - 1]))).hide().fadeIn('slow')
-        });
+
+        $.get('/tweets')
+          .done((data) => {
+            $('.feed').prepend(createTweetElement((data[data.length - 1]))).hide().fadeIn('slow');
+          });
       });
 
-      formReset()
+      formReset();
     }
   });
 
-
-  // Focus on tweet text area when write a new tweet icon is selected
-  $('.fa-solid.fa-angles-down').on('click', () => {
-  
-    $('#tweet-text').focus()
-  
-  })
-
-  
   loadTweets();
 });
